d5196f55f827af0356bf125b552f2a2dc21f179b
David Rachwalik <david.rachwalik@outlook.com>
2023-02-07 05:03:16 -0600

    passing the lint check

--- client-angular/src/app/app.component.spec.ts
+++ client-angular/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client-angular'`, () => {
+  it("should have as title 'client-angular'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('client-angular');
@@ -30,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'client-angular app is running!',
+    );
   });
 });
