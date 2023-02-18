6f75db6be97d4d6a7618e49f751d482f4cb3c3f9
David Rachwalik <david.rachwalik@outlook.com>
2023-02-18 14:36:20 -0600

    backing up experimental notes

--- client-angular/src/app/shared/redux/entities/product/product.selectors.ts
+++ client-angular/src/app/shared/redux/entities/product/product.selectors.ts
@@ -1,10 +1,21 @@
 import { createFeatureSelector } from '@ngrx/store';
 
 import { adapter, State as ProductState } from './product.reducer';
+// import { State as AppState } from '../..';
+// import { adapter } from './product.reducer';
 
 // https://ngrx.io/guide/store/selectors#selecting-feature-states
 // AppState will have 'product' feature, then finds ProductState
 export const getProductState = createFeatureSelector<ProductState>('products');
+
+// Alternative is StoreModule.forFeature('root', reducers) after forRoot
+
+// export const getRootState = createFeatureSelector<AppState>('root');
+
+// export const getProductState = createSelector(
+//   getRootState,
+//   (state: AppState) => state.products,
+// );
 
 export const { selectIds, selectEntities, selectAll, selectTotal } =
   adapter.getSelectors(getProductState);
--- client-angular/src/app/shared/redux/redux.module.ts
+++ client-angular/src/app/shared/redux/redux.module.ts
@@ -3,19 +3,30 @@ import { isDevMode, NgModule } from '@angular/core';
 import { EntityDataModuleWithoutEffects } from '@ngrx/data';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+// import { RootStoreConfig, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { metaReducers, reducers } from '.';
+// import { metaReducers, reducers, State as AppState } from '.';
 import { entityConfig } from './entities/entity-metadata';
 import { ProductService } from './entities/product/product.service';
+
+// const rootStoreConfig: RootStoreConfig<AppState> = {
+//   initialState: {},
+//   metaReducers,
+//   // Most checks are enabled by default
+//   // runtimeChecks: {
+//   //   strictStateImmutability: true,
+//   //   strictActionImmutability: true,
+//   // },
+// };
 
 @NgModule({
   declarations: [],
   imports: [
     // https://ngrx.io/guide/store/metareducers
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    // StoreModule.forRoot(reducers, rootStoreConfig),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
     // https://ngrx.io/api/data | https://ngrx.io/api/data/EntityDataModuleWithoutEffects
