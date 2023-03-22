24b402544fa080eedc5e3067bf8dd39f4852351e
David Rachwalik <david.rachwalik@outlook.com>
2023-03-21 22:47:49 -0500

    PizzaDataService added to provider

--- client-angular/src/app/modules/ngrx-data-local/redux/redux.module.ts
+++ client-angular/src/app/modules/ngrx-data-local/redux/redux.module.ts
@@ -4,6 +4,7 @@ import { StoreModule } from '@ngrx/store';
 
 import { entityMetadata } from './entities/entity-metadata';
 import { PizzaDataLocalService } from './entities/pizza/pizza-local.service';
+import { PizzaDataService } from './entities/pizza/pizza.service';
 import { metaReducers, reducers } from './entities/reducers';
 
 @NgModule({
@@ -13,7 +14,7 @@ import { metaReducers, reducers } from './entities/reducers';
   ],
   exports: [StoreModule],
   // Use 'providers' for services to only be available within the module
-  providers: [PizzaDataLocalService],
+  providers: [PizzaDataService, PizzaDataLocalService],
 })
 export class DataLocalReduxModule {
   // @ngrx/data missing forFeature method for lazy loading
